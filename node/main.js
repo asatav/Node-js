36315efcba05c6291596c139276f52e19effc8c1
ASHISH <ashishsatav968@gmail.com>
2019-12-28 20:50:49 +0530

    first node js sample

---
+++ node/main.js
@@ -0,0 +1,14 @@
+var person=function(a,b){
+    var c=a+b
+    console.log("Addition of a &b="+c)
+}
+person(4,5);
+
+//arrow function
+
+var users=(a,b)=>{
+    var c=a+b;
+    console.log("Arrow addition"+c);
+
+}
+users(5,9);
